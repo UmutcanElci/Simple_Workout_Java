53324fa902948df0f906b27dac9e64a5bc25b963
UmutcanElci <umutcanelci@yaani.com>
2022-07-11 16:43:23 +0300

    Simple_Workout_Java

--- src/Workout/Simple_Workout.java
+++ src/Workout/Simple_Workout.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Simple_Workout {
     public static void main(String[] args) {
-
-
-
+        /*This repository is my own workout to learn GitHub and logic of coding.
+        * First I start with easy things to repeat
+        * Then continue with more complex things like search and sort
+        * And last of all I'm going to learn and workout here OOP*/
     }
 }
