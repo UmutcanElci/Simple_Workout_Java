75f2f90c2f0c6d484b0b8e65faa1685e080c7c62
UmutcanElci <umutcanelci@yaani.com>
2022-07-11 16:33:34 +0300

    Simple_Workout_Java

--- src/Workout/Simple_Workout.java
+++ src/Workout/Simple_Workout.java
@@ -7,7 +7,8 @@ public class Simple_Workout {
         //System.out.println("Hi");
         //Java program to check whether the given number is even or odd
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+
+        /* System.out.println("Enter a number: ");
 
         int number = input.nextInt();
         if(number % 2 == 0){
@@ -16,5 +17,20 @@ public class Simple_Workout {
         else{
             System.out.println("The number is odd!");
         }
+         */
+
+        //Java program to find the area of a triangle whose three sides are given
+
+        int a,h;
+        int area;
+        System.out.println("Enter first side : ");
+        a = input.nextInt();
+        System.out.println("Enter height : ");
+        h = input.nextInt();
+
+        area = (a * h) / 2;
+
+        System.out.println("Area of triangle is : " + area);
+
     }
 }
