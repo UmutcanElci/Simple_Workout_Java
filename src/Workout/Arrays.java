c806f8ab2d658c3c004e5595fe011ac3f1562645
UmutcanElci <umutcanelci@yaani.com>
2022-07-14 12:20:19 +0300

    Attempt 1

--- src/Workout/Arrays.java
+++ src/Workout/Arrays.java
@@ -1,5 +1,35 @@
 package Workout;
 
 public class Arrays {
-    
+    public static void printIntArray(int[] array){
+        for (int i = 0; i < array.length;i++) {
+            System.out.print(" " + i);
+        }
+    }
+
+    public static void printIntArraySum(int[] array){
+        int sum = 0;
+        for(int i = 0; i < array.length;i++){
+            sum += array[i];
+        }
+        System.out.println(sum);
+    }
+
+    public static void printAvgIntArray(int[] array){
+        int sum = 0;
+        int avg;
+        for(int i = 0; i < array.length;i++){
+            sum += array[i];
+        }
+        avg = sum / array.length;
+        System.out.println(avg);
+    }
+
+    public static void printMaxMinValueOfArray(int[] array){
+        int max = 0;
+        int min = 0;
+        for (int i = 0; i < array.length;i++){
+            
+        }
+    }
 }
--- src/Workout/Simple_Workout.java
+++ src/Workout/Simple_Workout.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Simple_Workout {
     public static void main(String[] args) {
-
+        int arr[] = new int[]{1,2,3,4,5};
+        Arrays.printAvgIntArray(arr);
     }
 }
