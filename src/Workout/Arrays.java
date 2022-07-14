0113271a23961d44eeb5a27d7694a9d2555f491e
UmutcanElci <umutcanelci@yaani.com>
2022-07-14 13:03:47 +0300

    Arrays

--- src/Workout/Arrays.java
+++ src/Workout/Arrays.java
@@ -26,10 +26,27 @@ public class Arrays {
     }
 
     public static void printMaxMinValueOfArray(int[] array){
-        int max = 0;
-        int min = 0;
+        int max = array[0];
+        int min = array[0];
         for (int i = 0; i < array.length;i++){
-            
+            //array[] = {4,7,3,1,9}
+            //4 < 4 false 7 < 4 false 3 < 4 true 1 < 3 true 9 < 1 false so min value is  1
+            if(array[i] < min){
+                min = array[i];
+            }
+            //4 > 4 false 7 > 4 true 3 > 7 false 1 > 7 false 9 > 7 true so max vale is 9
+            if(array[i] > max){
+                max = array[i];
+            }
         }
+        System.out.println("Max value of the array : "+ max);
+        System.out.println("Min value of the array : "+ min);
+    }
+
+    public static void printReversedIntArray(int[] array){
+        // let say our array[4, 2, 7, 1, 8, 5, 9]
+        int temp;
+        //temp is a holder
+
     }
 }
--- src/Workout/Simple_Workout.java
+++ src/Workout/Simple_Workout.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Simple_Workout {
     public static void main(String[] args) {
         int arr[] = new int[]{1,2,3,4,5};
-        Arrays.printAvgIntArray(arr);
+        Arrays.printMaxMinValueOfArray(arr);
     }
 }
