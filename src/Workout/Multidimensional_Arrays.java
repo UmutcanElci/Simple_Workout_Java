874d6bb766e4abf54a4417a567e1932df429b21f
UmutcanElci <umutcanelci@yaani.com>
2022-07-15 14:53:19 +0300

    2D Arrays

--- src/Workout/Multidimensional_Arrays.java
+++ src/Workout/Multidimensional_Arrays.java
@@ -38,13 +38,28 @@ public class Multidimensional_Arrays {
     //The thing I can't solve is when I call this method in my main program wants a array[][] with values I found one thing and that's not the actual solutions.
     //I give my array a value like[10][10] we give huge value cause when the program ask the row and col the values changed, and we can input the 2D array values.
 
-    public static void printfSumOfTwoMatrix(int[][] array1,int[][] array2,int[][] sum){
-
-        for(int i = 0; i< array1.length;i++){
-            for(int a = 0; a< array1.length;a++){
-
+    public static void printfSumOfTwoMatrix(int[][] array1,int[][] array2){
+        Scanner input = new Scanner(System.in);
+        int row = 0;
+        int col = 0;
+        row = input.nextInt();
+        col = input.nextInt();
+        int sum[][] = new int[row][col];
+        for(int i = 0; i< row;i++){
+            for(int a = 0; a< col;a++){
+                sum[i][a] = array1[i][a] + array2[i][a];
             }
         }
+        for (int i = 0; i < sum.length; i++) {
+            for (int a = 0; a < sum.length; a++) {
+                System.out.print(sum[i][a] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printListArray(String [][] names){
+
     }
     }
 
